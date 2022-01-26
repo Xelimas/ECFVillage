81cc7b45e9fd182e11e956a8d788e6c55a330a66
Xelimas <briesch.bastien@hotmail.fr>
2022-01-26 16:38:42 +0100

    ECFVillage v12 CSS

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Un compte utilise deja cette adresse mail !")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
